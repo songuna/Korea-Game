1eb15e307c70084a3f54bb04f4c82c31edaea57f
201601972-kimdongwook <138658065+songuna@users.noreply.github.com>
2023-07-12 16:01:08 +0900

    별찍기 3
    //    *
    //   ***
    //  *****
    // *******
    //*********

--- zerocho 별찍기.js
+++ zerocho 별찍기.js
@@ -38,3 +38,22 @@ for (var i = 1; i <= 5; i++) {
     }
     console.log(str);
 }
+
+// 출력예시 3번
+//    *
+//   ***
+//  *****
+// *******
+//*********
+for (var i = 1; i <= 9; i++) {
+    if (i % 2 === 1) {
+        var str = '';
+        for (var j = 1; j <= (9 - i) / 2; j++) {
+            str = str + ' ';
+        }
+        for (var j = 1; j <= i; j++) {
+            str = str + '*';
+        }
+        console.log(str);
+    }
+}
