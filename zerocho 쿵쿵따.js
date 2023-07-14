151f75e2f05399b2a833bc9feb48613bd7557124
201601972-kimdongwook <138658065+songuna@users.noreply.github.com>
2023-07-14 18:01:17 +0900

    zercho 쿵쿵따 만들기
    prompt 함수 사용 취소 누르면 창 안넘어감 HTML코드를 JS로 코드 변경 후 완성!

--- zerocho 쿵쿵따.js
+++ zerocho 쿵쿵따.js
@@ -1,11 +1,24 @@
-//let promptObj = prompt('몇명이 참가하시나요?', 'default');
+let promptObj = prompt('몇명이 참가하시나요?');
+console.log(promptObj);
+
+if (promptObj !== null) {
+    const title = document.querySelector(".title");
+    const label = document.createElement('label');
+    const input = document.createElement('input');
+    const btn = document.createElement('button');
+    label.innerText = '쿵쿵따 게임 시작하기'
+    input.setAttribute('type', 'text')
+    input.setAttribute('id', 'Game')
+    btn.innerText = " 확인"
+    title.append(label, input, btn)
 
 
 
-const click = document.querySelector(".button");
-click.addEventListener("click", function () {
-    const Game = document.querySelector("#Game");
-    if (Game.value.length >= 3) {
-        alert("세글자 미만이여야 합니다. 다시 입력해주세요");
-    }
-});
+    btn.addEventListener("click", function () {
+        const Game = document.querySelector("#Game");
+        if (Game.value.length > 3) {
+            alert("세글자 미만이여야 합니다. 다시 입력해주세요");
+        }
+    });
+
+}
