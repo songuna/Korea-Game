cecd3e4606c605b78856c20247bdc700eb92d61d
201601972-kimdongwook <138658065+songuna@users.noreply.github.com>
2023-07-17 14:40:16 +0900

    Update zerocho 쿵쿵따.js
    JS 이해하고 주석달기

--- zerocho 쿵쿵따.js
+++ zerocho 쿵쿵따.js
@@ -1,10 +1,22 @@
 let promptObj = prompt('몇명이 참가하시나요?');
+// 홈페이지 첫 화면 뜨기 전에 질문 등장!
+// 질문에 답변하면 메인화면 등장
+// 질문 취소하면 메인화면 안뜸
 
+
+
+// 자바스크립트로 html만들기
+// 메인화면 만들기
+//조건문 써서 3글자 미만이면 질문 등장
 if (promptObj !== null) {
     const title = document.querySelector(".title");
+    // 변수명 선언해서 html에서 title 불러오기 (class명 불러오기)
     const label = document.createElement('label');
+    // 변수명 선언해서 html에서 label 불러오기 (id 불러오기)
     const input = document.createElement('input');
+    // 변수명 선언해서 html에서 input 불러오기 (id 불러오기)
     const btn = document.createElement('button');
+    // 변수명 선언해서 html에서 bytton 불러오기 (id 불러오기)
     label.innerText = '쿵쿵따 게임 시작하기'
     input.setAttribute('type', 'text')
     input.setAttribute('id', 'Game')
